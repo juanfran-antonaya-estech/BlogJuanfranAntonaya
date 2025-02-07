eee1594ebbcfa2e66957c1ac0871d97e1e4a5b2b
Juan Francisco <juanfran.antonaya@escuelaestech.es>
2025-02-07 17:23:58 +0100

    Añadidos factories para Comment, Post y Product
    Se han creado las factories necesarias para generar datos ficticios de Comment, Post y Product. Estas incluyen la lógica para poblar campos relacionados como IDs de usuarios y otras propiedades generadas con Faker.

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->text(20),
+            'description' => $this->faker->text(200),
+            'quantity' => $this->faker->numberBetween(1, 100),
+            'status' => $this->faker->boolean(),
+            'seller_id' => User::all()->random()->id,
+        ];
+    }
+}
