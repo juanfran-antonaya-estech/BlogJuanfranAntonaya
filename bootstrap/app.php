0cb92dd5c5ae7a3afcfb7562a2e97498256e9381
Juan Francisco <juanfran.antonaya@escuelaestech.es>
2025-02-17 20:21:14 +0100

    API implementada

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,11 +7,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
+            'jwt.auth' => 'Tymon\JWTAuth\Middleware\GetUserFromToken',
+            'jwt.refresh' => 'Tymon\JWTAuth\Middleware\RefreshToken',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
